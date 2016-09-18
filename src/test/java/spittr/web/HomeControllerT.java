cb68414b38096da89dfd546ee1645afa3c2f6f6f
lee <daniellee4421@gmail.com>
2016-09-18 23:48:12 +0900

    <158 page> SpitterController.processRegistration 추가

--- src/test/java/spittr/web/HomeControllerT.java
+++ src/test/java/spittr/web/HomeControllerT.java
@@ -13,7 +13,7 @@ import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.servlet.view.InternalResourceView;
 import static org.mockito.Mockito.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 import spittr.Spittle;
 import spittr.data.SpittleRepository;
@@ -32,8 +32,52 @@ public class HomeControllerT {
 		SpittleRepository mockRepository = mock(SpittleRepository.class);
 		when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedSpittles);
 		SpittleController controller = new SpittleController(mockRepository);
-		MockMvc mockMvc = standaloneSetup(controller).setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp")).build();
-		mockMvc.perform(get("/spittles")).andExpect(view().name("spittles")).andExpect(model().attributeExists("spittleList")).andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
+		
+		MockMvc mockMvc = standaloneSetup(controller)
+			.setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp"))
+			.build();
+		
+		mockMvc.perform(get("/spittles"))
+			.andExpect(view().name("spittles"))
+			.andExpect(model().attributeExists("spittleList"))
+			.andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
+	}
+	
+	
+	@Test
+	public void shouldShowPagedSpittles() throws Exception {
+		List<Spittle> expectedSpittles = createSpittleList(50);
+		SpittleRepository mockRepository = mock(SpittleRepository.class);
+		when(mockRepository.findSpittles(238900, 50)).thenReturn(expectedSpittles);
+
+		SpittleController controller = new SpittleController(mockRepository);
+		MockMvc mockMvc = standaloneSetup(controller)
+			.setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp"))
+			.build();
+
+		mockMvc.perform(get("/spittles?max=238900&count=50"))
+			.andExpect(view().name("spittles"))
+			.andExpect(model().attributeExists("spittleList"))
+			.andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
+	}
+	
+	
+	   /*
+	    * Testing a request for a Spittle with ID specified in a path variable
+	    * */
+	@Test
+	public void testSpittle() throws Exception {
+		Spittle expectedSpittle = new Spittle("Hello", new Date());
+	    SpittleRepository mockRepository = mock(SpittleRepository.class);
+	    when(mockRepository.findOne(12345)).thenReturn(expectedSpittle);
+	    
+	    SpittleController controller = new SpittleController(mockRepository);
+	    MockMvc mockMvc = standaloneSetup(controller).build();
+
+	    mockMvc.perform(get("/spittles/12345"))
+	      .andExpect(view().name("spittle"))
+	      .andExpect(model().attributeExists("spittle"))
+	      .andExpect(model().attribute("spittle", expectedSpittle));
 	}
 
 	private List<Spittle> createSpittleList(int count) {
